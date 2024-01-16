6b4141e764d640da66677bdf0936a36f4919d5e1
Munachimso <Victorycollins31@gmail.com>
2024-01-16 19:29:21 +0100

    New: Set up express server with typescript

--- src/server.ts
+++ src/server.ts
@@ -1,2 +1,12 @@
-console.log('hello world');
+import express, {Request, Response} from "express";
+
+const app = express();
+const port = 9000;
+
+app.get("/",(req: Request, res:Response) =>{
+    return res.send("hello world");
+});
+app.listen(port,()=>{
+    console.log('server is running on port'+ port);
+})
 
